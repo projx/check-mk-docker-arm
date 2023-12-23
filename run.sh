c69c7df9c69a4c9e5bb51ceb87d87f2bdd2ec57a
PRJX-UK <1296359+projx@users.noreply.github.com>
2023-12-23 23:19:17 +0100

    Updated

--- run.sh
+++ run.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
-sleep infinity
+echo "             __              __                __              __    "
+# echo ".----.-----.|__|______.----.|  |--.-----.----.|  |--.--------.|  |--."
+# echo "|   _|  _  ||  |______|  __||     |  -__|  __||    <|        ||    < "
+# echo "|__| |   __||__|      |____||__|__|_____|____||__|__|__|__|__||__|__|"
+# echo "     |__|     "
+ if [ ! -d /omd/sites/mon ]; then
+   ./init.sh
+ fi
+ if ! id -g mon &>/dev/null; then
+     groupadd -g 1000 mon
+ fi
+ if ! id -u mon &>/dev/null; then
+     useradd -g 1000 -u 1000 -ms /bin/bash -d /omd/sites/mon mon
+ fi
+    usermod -aG mon www-data
+    usermod -aG omd mon
+    omd mv mon temp
+    omd mv temp mon
+    omd enable mon
+ #else
+ #    omd start mon
+ #    sleep infinity
+ omd start mon
+ sleep infinity
+#  fi
+
+
+#!/bin/bash
+
 # echo "             __              __                __              __    "
 # echo ".----.-----.|__|______.----.|  |--.-----.----.|  |--.--------.|  |--."
 # echo "|   _|  _  ||  |______|  __||     |  -__|  __||    <|        ||    < "
@@ -8,8 +36,7 @@ sleep infinity
 # if [ ! -d /omd/sites/mon ]; then
 #   ./init.sh
 # fi
-# if ! id mon &>/dev/null; then
-#     groupadd -g 1000 mon
+# if ! id mon &>/dev/null; then groupadd -g 1000 mon
 #     useradd -g 1000 -u 1000 -ms /bin/bash -d /omd/sites/mon mon
 #     usermod -aG mon www-data
 #     usermod -aG omd mon
